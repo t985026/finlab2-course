db0d7ad166354a26449b84da725c3040a552ff4f
t985026 <t985026@yahoo.com.tw>
2019-09-13 19:41:25 +0800

    update

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,7 +1,7 @@
 pip install jupyterlab
 
 echo ================= TALIB ===============
-python -m pip install setuptools Jinja2==2.10 joblib==0.13.2
+python -m pip install setuptools Jinja2==2.10.1 joblib==0.13.2
 python -m pip install eli5 pyfolio
 python -m pip install TA-Lib
 python -m pip install xgboost shap mlxtend seaborn
